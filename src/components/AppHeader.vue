5e65c595f3f61b15a68fbb1ab80c1a98e5d2fa91
Dynastiy <emmanuel00michael@gmail.com>
2023-02-07 16:07:34 +0100

    added header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container py-3">
+    <div class="app-header d-flex align-items-center justify-content-between">
+      <div class="w-100">
+        <span class="font-weight-bold">Buy Airtime and Data</span>
+      </div>
+      <div
+        class="d-flex align-items-center justify-content-end"
+        style="gap: 10px"
+      >
+        <img width="40%" src="@/assets/img/e-Naira-logo.png" alt="" />
+        <span role="button">
+          <i-icon icon="ic:outline-menu" width="30px" />
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
--- src/layouts/AppHomeLayout.vue
+++ src/layouts/AppHomeLayout.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
     <div id="main">
+      <AppHeader />
       <slot />
     </div>
   </div>
 </template>
 
 <script>
+import AppHeader from "@/components/AppHeader.vue";
 export default {
+  components: {
+    AppHeader,
+  },
   name: "HomeLayout",
 };
 </script>
